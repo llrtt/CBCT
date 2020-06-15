eade57069df43bf608bfe994936f2c24b9028d28
llrt <2267964591@qq.com>
2020-06-15 11:22:00 +0800

    通用

--- dic.py
+++ dic.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pathlib
 import os
 
-paths = pathlib.Path('D:\\CBCT to sCT')
+paths = pathlib.Path('CBCT to sCT')
 all_CT_paths = list(paths.glob('*'))
 all_CT_paths = [str(path) for path in all_CT_paths]
 sub_paths = {0}
@@ -23,15 +23,21 @@ for i in (all_CT_paths):
         c_paths = list(c_paths.glob('*'))
         c_paths = [str(p) for p in c_paths]
         for CT in c_paths:
-            if(CT.find('dir')):
+            print(CT)
+            try:
+                dcm = pydicom.read_file(CT, force=True)
+                dcm.file_meta.TransferSyntaxUID = pydicom.uid.ImplicitVRLittleEndian
+                dir_name = CT.replace('CBCT to sCT','CBCT和CT图的二维图像')
+                dir_name += ".png"
+                plt.figure()
+                plt.imshow(dcm.pixel_array, cmap='gray')
+                plt.xticks([])
+                plt.yticks([])
+            except  AttributeError:
                 continue
-            dcm = pydicom.read_file(CT, force=True)
-            dcm.file_meta.TransferSyntaxUID = pydicom.uid.ImplicitVRLittleEndian
-            dir_name = CT.replace('CBCT to sCT','CBCT和CT图的二维图像')
-            dir_name = dir_name.replace('DCM','png')
-            plt.imshow(dcm.pixel_array, cmap='gray')
-            if(not(os.path.exists(dir_name))):
-                plt.savefig(dir_name)
+            else:
+                if(not(os.path.exists(dir_name))):
+                    plt.savefig(dir_name)
             
 
 # plt.figure()
